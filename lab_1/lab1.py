4180e978b3f5fd5904d81addc1658148c25ce7ff
kirill <tolkun_14@inbox.ru>
2020-09-28 22:34:31 +0300

    Add function 'mclaren_marsaglia_generator()'

--- lab_1/lab1.py
+++ lab_1/lab1.py
@@ -1,3 +1,6 @@
+import math
+
+
 def linear_congruential_generator(x, alpha, c, m):
     while (True):
         x = (alpha * x + c) % m
@@ -8,3 +11,13 @@ def multiplexial_congruential_generator(x, alpha, m):
     generator = linear_congruential_generator(x, alpha, 0, m)
     while (True):
         yield next(generator)
+
+
+def mclaren_marsaglia_generator(x_generator, y_generator, k):
+    V = [next(x_generator) for _ in range(k)]
+    while (True):
+        X = next(x_generator)
+        Y = next(y_generator)
+        j = math.floor(k * Y)
+        yield V[j]
+        V[j] = X
