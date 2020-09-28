039f16709f75e849703f854641ad50c2b6b465b4
kirill <tolkun_14@inbox.ru>
2020-09-28 22:34:02 +0300

    Add function 'multiplexial_congruential_generator()'

--- lab_1/lab1.py
+++ lab_1/lab1.py
@@ -2,3 +2,9 @@ def linear_congruential_generator(x, alpha, c, m):
     while (True):
         x = (alpha * x + c) % m
         yield x / m
+
+
+def multiplexial_congruential_generator(x, alpha, m):
+    generator = linear_congruential_generator(x, alpha, 0, m)
+    while (True):
+        yield next(generator)
